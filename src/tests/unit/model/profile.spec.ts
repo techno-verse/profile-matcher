36f7e4ab1c6b96b356a8e754c8e39b09d4eabb70
shreyaspatel7 <shreyas7p@gmail.com>
2020-06-15 14:30:00 -0400

    adding profile test

--- src/tests/unit/model/profile.spec.ts
+++ src/tests/unit/model/profile.spec.ts
@@ -1,16 +1,31 @@
 import { Profile } from "../../../model/profile"
 import { expect } from 'chai';
 
-describe('Profile Model Tests', () => {
+describe('Profile Model Positive Tests', () => {
 
     it('Test Profile Model Can Set Values', () => {
         let name: string = "Batman"
         let distance: number = 0
+        let industryScore: number = 1
+        let jobTitleScore: number = 1
+        let profile: Profile = new Profile(name, distance, industryScore, jobTitleScore)
+        expect(profile.name).to.be.eq(name)
+        expect(profile.distance).to.be.eq(distance)
+        expect(profile.score).to.be.eq(100)
+    });
+});
+
+
+describe('Profile Model Negative Tests', () => {
+
+    it('Test Profile Model Can Set Values', () => {
+        let name: string = "Super Mans"
+        let distance: number = 100
         let industryScore: number = 0
         let jobTitleScore: number = 0
         let profile: Profile = new Profile(name, distance, industryScore, jobTitleScore)
-        console.log(profile.name)
-        console.log(profile.distSc)
-        console.log(profile.score)
+        expect(profile.name).to.be.eq(name)
+        expect(profile.distance).to.be.eq(distance)
+        expect(profile.score).to.be.eq(0)
     });
 });
